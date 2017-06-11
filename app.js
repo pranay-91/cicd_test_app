34b009bec2754fb42f6231d23d0e4aea372c7b2a
Pranay Pradhananga <pronoy19114@gmail.com>
2017-06-12 05:16:10 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+var exports = module.exports = {};
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+})
+
+var server = app.listen(3000, function() {
+  console.log('App is listening on port 3000');
+})
+
+exports.closeServer = function() {
+  server.close();
+};
---
+++ spec/helloWorldSpec.js
@@ -0,0 +1,27 @@
+var request = require("request");
+var helloWorldApp = require("../app.js");
+var base_url ="http://localhost:3000/"
+
+describe("Hello World Server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+        request(base_url, function(error, response, body) {
+          // TODO: ADD fix for error or request exception
+          // expect(error).toBe(null); doesnt seem to work. Using --captureExceptions when running jasmine-node
+
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    });
+    it("returns Hello World", function(done) {
+      request(base_url, function(error, response, body) {
+        expect(body).toEqual('Hello World');
+
+        // Closing server from app.js
+        console.log('Closing server..');
+        helloWorldApp.closeServer();
+        done();
+      })
+    } )
+  });
+});
